5940887bd66dd6dcbfe93e468db66b4425c554c7
Dusan Klinec <dusan.klinec@gmail.com>
2015-12-07 08:54:37 +0100

    coverity build - dump coverity analysis tool log

--- build-coverity.sh
+++ build-coverity.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
+PIR=`pwd`
+echo "Script directory: $DIR, current directory: $PIR"
 
 cd build
 ant clean openfire test plugins
 
 echo "Build done, switching directory to $DIR"
 cd "$DIR"
+
+echo "Cov log"
+cat cov-int/build-log.txt 
+
+echo "DONE"
 
